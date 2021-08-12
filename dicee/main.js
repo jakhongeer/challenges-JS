67be072d5d9c68f5de9cdf0a333fd99067cddcc3
Jakhongir <jahongirnagmatov@gmail.com>
2021-08-12 11:04:47 +0500

    complete dice challenge

--- dicee/main.js
+++ dicee/main.js
@@ -1,9 +1,23 @@
-function randomNumber(){
-    var firstPlayerDice = Math.floor(Math.random()*7);
-    var secondPlayerDice = Math.floor(Math.random()*7);
-    return {
-        firstPlayerDice,
-        secondPlayerDice
-    };
+var firstPlayerDice = Math.floor(Math.random() * 6 + 1);
+var secondPlayerDice = Math.floor(Math.random() * 6 + 1);
+
+document.getElementById(
+  "firstPlayer"
+).src = `images/dice${firstPlayerDice}.png`;
+document.getElementById(
+  "secondPlayer"
+).src = `images/dice${secondPlayerDice}.png`;
+
+
+
+if (firstPlayerDice > secondPlayerDice) {
+  document.getElementById("crown1").style.visibility = "visible"
+  document.getElementById("title").innerHTML = "Player 1 wins!";
+  
+} else if (firstPlayerDice < secondPlayerDice) {
+  document.getElementById("crown2").style.visibility = "visible"
+  document.getElementById("title").innerHTML = "Player 2 wins!";
+} else {
+  document.getElementById("title").innerHTML = "Draw!";
 }
-randomNumber()
+
