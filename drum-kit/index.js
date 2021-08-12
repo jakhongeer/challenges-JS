a13c7db662add085e67019acbefd3ae2a63309e3
Jakhongir <jahongirnagmatov@gmail.com>
2021-08-12 12:04:07 +0500

    when button clicks it plays the same sound for all

---
+++ drum-kit/index.js
@@ -0,0 +1,13 @@
+numberOfDrums = document.querySelectorAll(".drum").length
+
+
+for (var i = 0; i < numberOfDrums; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        var audio = new Audio("sounds/snare.mp3")
+        audio.play();
+      });
+    
+}
+
+
+
