b423058da4b030a1f641a6c7b53a1185b81ade89
Jakhongir <jahongirnagmatov@gmail.com>
2021-08-12 13:26:29 +0500

    commented out the audio code

--- drum-kit/index.js
+++ drum-kit/index.js
@@ -3,11 +3,12 @@ numberOfDrums = document.querySelectorAll(".drum").length
 
 for (var i = 0; i < numberOfDrums; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        var audio = new Audio("sounds/snare.mp3")
-        audio.play();
+
       });
     
 }
 
 
 
+// var audio = new Audio("sounds/snare.mp3")
+// audio.play();
