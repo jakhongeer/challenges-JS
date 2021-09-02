590f2e7f9ee283484eddfb9b4da1bd2acc8208e7
Jakhongir <jahongirnagmatov@gmail.com>
2021-09-02 23:25:37 +0500

    add default into switch func

--- drum-kit/index.js
+++ drum-kit/index.js
@@ -41,6 +41,9 @@ for (var i = 0; i <numberOfDrums; i++) {
         var kickbass = new Audio("sounds/crash.mp3");
         kickbass.play()
         break;
+
+      default:
+        console.log(buttonInnerHTML)
     }
 
   })
