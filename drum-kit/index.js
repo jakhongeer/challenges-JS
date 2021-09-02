aa971224227b3833d05f1ff791197a9a52bff35d
Jakhongir <jahongirnagmatov@gmail.com>
2021-09-02 23:17:42 +0500

    when the specific button clicks, specific sound plays

--- drum-kit/index.js
+++ drum-kit/index.js
@@ -1,14 +1,25 @@
 numberOfDrums = document.querySelectorAll(".drum").length
 
+for (var i = 0; i <numberOfDrums; i++) {
 
-for (var i = 0; i < numberOfDrums; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-
-      });
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     
+    var buttonInnerHTML = this.innerHTML
+    
+    switch(buttonInnerHTML) {
+      case 'w':
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play()
+        break;
+
+      case 'a':
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play()
+        break;
+    }
+
+  })
+
 }
 
 
-
-// var audio = new Audio("sounds/snare.mp3")
-// audio.play();
