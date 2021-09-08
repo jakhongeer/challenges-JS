1adf8d8a7d82575fa2f5dd89ff8a4e7f268655c4
Jakhongir <jahongirnagmatov@gmail.com>
2021-09-08 18:55:49 +0500

    play more with jQuery (.text, .click, .addClass and etc.)

--- jQuery/index.js
+++ jQuery/index.js
@@ -2,3 +2,12 @@ $("h1").css("color", "blue");
 
 
 $("button").addClass("button2")
+
+$("h1").text("How are you doing?")
+
+$("button").click(function() {
+    $("button").css("color", "red")
+})
+$("button").click(function () {
+    $("h1").css("color", "purple")
+})
