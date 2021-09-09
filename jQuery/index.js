c52033566f19a8d6c32d7eddbccd0275ed804432
Jakhongir <jahongirnagmatov@gmail.com>
2021-09-09 11:15:34 +0500

    before, after, toggle and etc. methods in jQuery

--- jQuery/index.js
+++ jQuery/index.js
@@ -6,7 +6,7 @@ $("button").addClass("button2")
 $("h1").text("How are you doing?")
 
 $("button").click(function() {
-    $("button").css("color", "red")
+    $("button").css("color", "yellow")
 })
 $("button").click(function () {
     $("h1").css("color", "purple")
@@ -16,3 +16,12 @@ $("input").keypress(function(event) {
     $("h1").text(event.key)
 })
 
+$("h2").before("<h3>You are strong man.</h3>")
+
+$("h2").after("<h3>I believe you.</h3>")
+//.append() .prepend()
+
+$(".btn").on("click", function() {
+    $("h2").toggle()
+})
+//.hide() .show()
